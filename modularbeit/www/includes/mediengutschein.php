0c6f32f45112dd389473bc3c030d8f1a95ac36f3
KlausBuderer <67792948+KlausBuderer@users.noreply.github.com>
2021-08-10 07:34:41 +0200

    Eltern Screens erstellt

---
+++ modularbeit/www/includes/mediengutschein.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'header.php';
+require_once 'navbar_eltern.php';
+require_once 'sidebar_eltern.php';
+
+
+?>
+
+
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+  <h1 class="h2">Mediengutschein Verwaltung</h1>
+</div>
+
+<a type="button" href="eltern_uebersicht_template.php" class="btn btn-outline-secondary">Zurück</a>
+
+
+<?php require_once 'footer.php'?>
