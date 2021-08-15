af678e24dd7a6189cf356ff1c8982e6ba0702223
Bruno Thurnherr <bruno.thurnherr@it-wissen.ch>
2021-08-15 08:04:59 +0200

    FTP Test

--- modularbeit/www/pdftest.php
+++ modularbeit/www/pdftest.php
@@ -5,10 +5,10 @@ $pdf=new FPDF();
  $pdf->AddPage();
  $pdf->SetFont('Arial','B',40);
  $pdf->Cell(20,10,'Medien-Gutschein ');
- $pdf->SetFont('Arial','B',25);
+ $pdf->SetFont('Arial','B',15);
  $pdf->Cell(120,120,'Herzlichen Gratulation zu deinem erworbenen wissen.');
- $pdf->Image('includes/images/spieler_gokart.png', '15', '100', '0', '0');
- 
- $pdf->Ln(18);
+ $pdf->Image('includes/images/spieler_gokart.png', '10', '80', '0', '0');
+ $pdf->SetFont('Arial','B',10);
+ $pdf->Cell(120,200,'Du darfst fuer diesen Gutschein');
  $pdf->Output(); 
 ?>
