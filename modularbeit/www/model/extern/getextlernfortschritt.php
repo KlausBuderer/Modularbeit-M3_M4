69d65f7c1670ca68839e52967a6d07ff2eca92e2
KlausBuderer <67792948+KlausBuderer@users.noreply.github.com>
2021-09-01 23:12:36 +0200

    JSON erweitert mit Bruno

--- modularbeit/www/model/extern/getextlernfortschritt.php
+++ modularbeit/www/model/extern/getextlernfortschritt.php
@@ -18,7 +18,7 @@ public function getausgabejson($fname, $lname)
     $conn = $connection->buildConnection();
 
     if (mysqli_connect_errno())echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    $sql = "SELECT `KK1`.`username` AS `Benutzername`, `KK1`.`fname` AS `Vorname`, `KK1`.`lname` AS `Nachname`, `KK2`.`lerneinheit` AS `Lerneinheit` FROM (`users` `KK1` JOIN `le_lernfortschritt` `KK2` ON (`KK1`.`id` = `KK2`.`userid`))
+    $sql = "SELECT `KK1`.`username` AS `Benutzername`, `KK1`.`fname` AS `Vorname`, `KK1`.`lname` AS `Nachname`, `KK2`.`lerneinheit` AS `Lerneinheit`, `KK2`.`punkteerreicht` AS `Punkte erreicht`, `KK2`.`vorzeitabbruch` AS `Vorzeitiger Abbruch` FROM (`users` `KK1` JOIN `le_lernfortschritt` `KK2` ON (`KK1`.`id` = `KK2`.`userid`))
      WHERE `KK1`.`fname` LIKE '%$fname%' AND `KK1`.`lname` LIKE '%$lname%';";
 
     $result = mysqli_query($conn, $sql);
